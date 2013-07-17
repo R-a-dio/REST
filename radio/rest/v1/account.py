93a5792c8b6f56576e058c5fd6b8042158a75799
HirotoKun <hiroto@hiroto.eu>
2013-07-17 08:36:37 +0100

    Added users to the API
    Default displays the first 50 users, todo: pagination

--- radio/rest/v1/account.py
+++ radio/rest/v1/account.py
@@ -1,14 +1,48 @@
-import web
+from radio.core.cursor import Cursor
 
-class account:
-    def GET(id):
-        pass
-    
-    def DELETE(id):
-        pass
-    
-    def PUT(id):
-        pass
-    
-    def POST(id):
-        pass
+from . import app
+from ..app import API
+
+@app.path("/users(?:/)?(\d+)?")
+class detail(object):
+
+    __metaclass__ = API
+
+    def GET(self, user_id):
+        with Cursor() as cur:
+            if user_id is not None:
+                count = cur.execute("""
+                SELECT id, user, djid, privileges
+                FROM users
+                WHERE id=%s
+                LIMIT 1
+                """, (user_id,))
+                
+                if count == 0:
+                    return {"errno": 1, "error": "user does not exist."}
+            else:
+                cur.execute("""
+                SELECT id, user, djid, privileges
+                FROM users
+                LIMIT 50
+                """)
+            ret = []
+            for id, user, djid, privileges, in cur:
+                ret.append({
+                    'id': id,
+                    'name': user,
+                    'dj_id': -1 if not djid else djid,
+                    'permissions': privileges,
+                    # New-style permissions
+                    #'permissions' {
+                    #    'accept': p_accept,  # accept pending songs
+                    #    'delete': p_delete, # database delete access
+                    #    'read': p_read, # database read access
+                    #    'edit': p_edit, # database update access
+                    #    'dj': p_dj, # able to use DJ proxy login
+                    #    'news': p_news, # able to post/edit news
+                    #    'admin': p_admin, # full admin console access (users)
+                    #    'dev': p_dev, # experimental console access (alerts, maintenance, relays)
+                    #},
+                }) 
+            return ret
