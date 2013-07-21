d1bcfea0bfed40bc8a60a590d86961c5ed2dcfc5
HirotoKun <hiroto@hiroto.eu>
2013-07-21 21:52:38 +0100

    Started pagination additions to queue
    In addition, the previous way this was done isnt actually the correct way to do REST.
    It's an argument to the resource, not the resource itself
    (now, if it fetched the nth resource in the queue...).

--- radio/rest/v1/queue.py
+++ radio/rest/v1/queue.py
@@ -1,4 +1,5 @@
 from radio.core.cursor import Cursor
+from flask import request
 
 from . import app
 from ..app import API
@@ -8,21 +9,28 @@ class Queue(object):
 
     @app.get("/queue/")
     def peek_top(self):
-        return self.iter(limit=5)
+        limit = request.args.get('limit', 5)
+        offset = request.args.get('offset', 0)
+        return self.iter(limit, offset)
 
-    @app.get("/queue/<int:limit>/")
-    def iter(self, limit):
+    def iter(self, limit, offset):
         """
-        GET /queue[/<limit, int>/]
+        GET /queue[/[?limit=<int:limit>[&offset=<int:offset>]]]
 
-        Returns the next <limit> songs in the queue.
+        Returns the next <limit> songs in the queue,
+        with <offset> from the start for pagination.
+        TODO: metadata on JSON to return the next/previous URL
+
+        `"metadata" : { "next" : <url> }`
         """
         if not limit:
             limit = 5
         elif int(limit) > 25:
             return {"error": "request too large"}
+        if not offset:
+            offset = 0
         with Cursor() as cur:
-            cur.execute("""
+            count = cur.execute("""
                 SELECT meta,
                        UNIX_TIMESTAMP(time) AS timestr,
                        type,
@@ -30,8 +38,12 @@ class Queue(object):
                        length
                 FROM queue
                 ORDER BY timestr ASC
-                LIMIT {:d}
-            """.format(int(limit)))
+                LIMIT %s, %s
+            """, (int(offset), int(limit)))
+
+            if not count:
+                return {"error": "no queue available"}
+
             res = {'queue': []}
             queue = res['queue']
             for meta, timestr, type, trackid, length in cur:
@@ -42,7 +54,5 @@ class Queue(object):
                     'length': length, # length in seconds.
                     'type': type # internal
                 })
-            else:
-                return {"error": "no queue available"}
 
             return res
