9d907296b879dcc16abe8b79a40613b3ef08e331
HirotoKun <hiroto@hiroto.eu>
2013-07-21 11:25:52 +0100

    Changed the permissions system in the users API
    This change breaks existing dev environments; get a fresh backup of the r/a/dio database to test on.

--- radio/rest/v1/account.py
+++ radio/rest/v1/account.py
@@ -3,44 +3,90 @@ from radio.core.cursor import Cursor
 from . import app
 from ..app import API
 
-class detail(object):
+class Users(object):
     __metaclass__ = API
 
-    def GET(self, user_id):
+    @app.get("/users/<int:user_id>/")
+    def get_user(self, user_id):
         with Cursor() as cur:
-            if user_id is not None:
-                count = cur.execute("""
-                SELECT id, user, djid, privileges
-                FROM users
-                WHERE id=%s
+            count = cur.execute("""
+                SELECT
+                    u.id, u.user, u.djid,
+                    p.accept, p.delete, p.read,
+                    p.edit, p.dj, p.news,
+                    p.admin, p.dev
+                FROM
+                    users as u
+                JOIN
+                    permissions as p
+                ON
+                    u.id = p.id
+                WHERE u.id=%s
                 LIMIT 1
                 """, (user_id,))
 
-                if count == 0:
-                    return {"errno": 1, "error": "user does not exist."}
-            else:
-                cur.execute("""
-                SELECT id, user, djid, privileges
-                FROM users
+            if count == 0:
+                return {"errno": 1, "error": "user does not exist."}
+
+            for id, user, djid, \
+                p_accept, p_delete, p_read, p_edit, p_dj, p_news, \
+                p_admin, p_dev in cur:
+                ret = {
+                    'id': id,
+                    'name': user,
+                    'dj_id': -1 if not djid else djid,
+                    #'permissions': privileges,
+                    # New-style permissions
+                    'permissions': {
+                        'accept': p_accept,  # accept pending songs
+                        'delete': p_delete, # database delete access
+                        'read': p_read, # database read access
+                        'edit': p_edit, # database update access
+                        'dj': p_dj, # able to use DJ proxy login
+                        'news': p_news, # able to post/edit news
+                        'admin': p_admin, # full admin console access (users)
+                        'dev': p_dev, # experimental console access (alerts, maintenance, relays)
+                    },
+                }
+            return {"user" : ret}
+
+    @app.get("/users/")
+    def get_all_users(self):
+        with Cursor() as cur:
+            cur.execute("""
+                SELECT
+                    u.id, u.user, u.djid,
+                    p.accept, p.delete, p.read,
+                    p.edit, p.dj, p.news,
+                    p.admin, p.dev
+                FROM
+                    users as u
+                JOIN
+                    permissions as p
+                ON
+                    u.id = p.id
                 LIMIT 50
                 """)
             ret = []
-            for id, user, djid, privileges, in cur:
+            for id, user, djid, \
+                p_accept, p_delete, p_read, p_edit, p_dj, p_news, \
+                p_admin, p_dev in cur:
                 ret.append({
                     'id': id,
                     'name': user,
                     'dj_id': -1 if not djid else djid,
-                    'permissions': privileges,
+                    #'permissions': privileges,
                     # New-style permissions
-                    #'permissions' {
-                    #    'accept': p_accept,  # accept pending songs
-                    #    'delete': p_delete, # database delete access
-                    #    'read': p_read, # database read access
-                    #    'edit': p_edit, # database update access
-                    #    'dj': p_dj, # able to use DJ proxy login
-                    #    'news': p_news, # able to post/edit news
-                    #    'admin': p_admin, # full admin console access (users)
-                    #    'dev': p_dev, # experimental console access (alerts, maintenance, relays)
-                    #},
+                    'permissions': {
+                        'accept': p_accept,  # accept pending songs
+                        'delete': p_delete, # database delete access
+                        'read': p_read, # database read access
+                        'edit': p_edit, # database update access
+                        'dj': p_dj, # able to use DJ proxy login
+                        'news': p_news, # able to post/edit news
+                        'admin': p_admin, # full admin console access (users)
+                        'dev': p_dev, # experimental console access (alerts, maintenance, relays)
+                    },
                 })
-            return ret
+            return {"users" : ret}
+
